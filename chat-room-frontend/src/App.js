83af0875ec1572f37f82d4bbcf373ad77225b4c5
racheleckenrod <rachel@racheleckenrod.com>
2023-10-22 16:22:15 -0400

    still testing formatting of fullstack

--- chat-room-frontend/src/App.js
+++ chat-room-frontend/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        // Wrapping the AppContent component with AuthProvider to provide authentication context
+        <AuthProvider>
+          <AppContent />  {/* This is where the main content of the application will be rendered */}
+        </AuthProvider>
   );
 }
 
---
+++ chat-room-frontend/src/components/Signup.js
@@ -0,0 +1,43 @@
+import React, {createContext, useState, useEffect} from "react";
+
+export const AuthContext = createContext()
+
+export const AuthProvider = ({children}) => {
+    // state variables
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [token, setToken] = useState(null);
+    const [username, setUsername] = useState(null);
+
+    useEffect(() => {
+        // Retrieve token and username from local storafe
+        const storedToken = localStorage.getItem('token')
+        const storedUsername = localStorage.getItem('username')
+
+        // If a token exsists in local storage, update state variables
+        if(storedToken) {
+            setToken(storedToken)
+            setIsAuthenticated(true)
+        }
+
+        if (storedUsername) {
+            setUsername(storedUsername)
+        }
+    })
+
+    return (
+        <AuthContext.Provider
+        value = {{
+            isAuthenticated,
+            setIsAuthenticated,
+            token,
+            setToken,
+            username,
+            setUsername
+        }}
+        >
+            {children} {/* Render children components */}
+        </AuthContext.Provider>
+    )    
+
+
+}
